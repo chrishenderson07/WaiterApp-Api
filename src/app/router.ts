aab123d6a6a7cc66a3011dc0056a0e9cc98f695e
chrishenderson07 <chris.hend012@gmail.com>
2024-01-27 08:33:26 -0300

    create products work

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,26 +1,36 @@
-import { Router } from 'express'
+import path from 'node:path'
 
+import { Router } from 'express'
+import multer from 'multer'
+
+import { listCategories } from './useCases/categories/listCategories'
+import { createCategory } from './useCases/categories/createCategory'
+import { listProducts } from './useCases/products/listProducts'
+import { createProduct } from './useCases/products/createProducts'
 export const router = Router()
 
-//List categories
-router.get('/categories', (req, res) => {
-	res.send('Lista de categorias')
+const upload = multer({
+	storage: multer.diskStorage({
+		destination(req, file, callback) {
+			callback(null, path.resolve(__dirname, '..', '..', 'uploads'))
+		},
+		filename(req, file, callback) {
+			callback(null, `${Date.now()}-${file.originalname}`)
+		},
+	}),
 })
+
+//List categories
+router.get('/categories', listCategories)
 
 //Create category
-router.post('/categories', (req, res) => {
-	res.send('Criando categoria')
-})
+router.post('/categories', createCategory)
 
 //List products
-router.get('/products', (req, res) => {
-	res.send('Lista de produtos')
-})
+router.get('/products', listProducts)
 
 //Create product
-router.post('/products', (req, res) => {
-	res.send('Criando produto')
-})
+router.post('/products', upload.single('image'), createProduct)
 
 //Get products by category
 router.get('/categories/:categoryId/products', (req, res) => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import path from 'node:path'
+
+import { router } from './app/router'
 
 //A url e porta do mongoose é a mesma que definimos na config do MongoDB no docker
 mongoose
@@ -7,6 +10,13 @@ mongoose
 	.then(() => {
 		const app = express()
 		const port = 3001
+
+		app.use(
+			'/uploads',
+			express.static(path.resolve(__dirname, '..', 'uploads')),
+		)
+		app.use(express.json())
+		app.use(router)
 
 		app.listen(port, () => {
 			console.log(`Server is running on port ${port}`)
