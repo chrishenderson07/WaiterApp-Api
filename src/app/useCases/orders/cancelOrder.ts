c5775971870e94ee81d43727f71fc7583265ad04
chrishenderson07 <chris.hend012@gmail.com>
2024-01-29 08:50:36 -0300

    Cancel order

--- src/app/useCases/orders/cancelOrder.ts
+++ src/app/useCases/orders/cancelOrder.ts
@@ -6,7 +6,6 @@ export async function cancelOrder(req: Request, res: Response) {
 	try {
 		const { orderId } = req.params
 		await Order.findByIdAndDelete(orderId)
-
 		res.sendStatus(204)
 	} catch (error) {
 		console.error(error)
